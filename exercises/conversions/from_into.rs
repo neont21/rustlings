adffacf17ab21c99ce55bd4f3a5ef44bbf3b0675
Peter J <peter.j@kakao.com>
2020-10-01 04:39:12 +0900

    complete exercises/conversions
    Signed-off-by: Peter J <peter.j@kakao.com>

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -18,7 +18,6 @@ impl Default for Person {
     }
 }
 
-// I AM NOT DONE
 // Your task is to complete this implementation
 // in order for the line `let p = Person::from("Mark,20")` to compile
 // Please note that you'll need to parse the age component into a `usize`
@@ -35,6 +34,26 @@ impl Default for Person {
 // Otherwise, then return an instantiated Person object with the results
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+
+        let v: Vec<&str> = s.split(',').collect();
+        if v.len() != 2 {
+            return Person::default();
+        }
+        let name = String::from(v[0]);
+        if name.len() == 0 {
+            return Person::default();
+        }
+        if let Ok(age) = v[1].parse::<usize>() {
+            Person {
+                name,
+                age,
+            }
+        } else {
+            Person::default()
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -10,7 +10,6 @@ struct Person {
     age: usize,
 }
 
-// I AM NOT DONE
 // Steps:
 // 1. If the length of the provided string is 0, then return an error
 // 2. Split the given string on the commas present in it
@@ -23,6 +22,22 @@ struct Person {
 impl FromStr for Person {
     type Err = String;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(String::from("empty string"));
+        }
+        
+        let v: Vec<&str> = s.split(',').collect();
+        let name = String::from(v[0]);
+        if name.len() == 0 {
+            Err(String::from("there is no name"))
+        } else if let Ok(age) = v[1].parse::<usize>() {
+            Ok(Person {
+                name,
+                age,
+            })
+        } else {
+            Err(String::from("the age is wrong value"))
+        }
     }
 }
 
